54507a2d23e4bd181de91147b60dc1b5060c2223
Lilly <lilly238913@mail.ru>
2012-10-28 12:43:58 +0400

    new proj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeW::Application.config.secret_token = 'fdb28f2b983986f10c433c3c4bad9a93f4447e902e2a425f8641c780c9408f3c046f0b81c200df6d4c6084e0de030d2c62088cd7dee381022738e8b5a74047a5'
